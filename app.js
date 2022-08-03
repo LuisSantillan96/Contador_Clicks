c52cd817b122a237a3dcfcb269e3598e981f755a
LuisSantillan96 <luistecisc@gmail.com>
2022-08-02 20:53:59 -0500

    Primer Commit

---
+++ app.js
@@ -0,0 +1,19 @@
+
+var contador = 0;
+
+// Seleccionamos la etiqueta segun e ID donde pintaremos el conteo
+const conteo = document.getElementById('conteo-act');
+
+// Aqui cada que el boton sea plsado se aumenta en 1
+const boton = document.getElementById('pulsador');
+
+boton.addEventListener('click', function(){
+    contador ++;
+    // Aqui pintamos el conteo segun el contador
+    conteo.textContent = `${contador}`;
+    //console.log(contador);
+});
+
+
+
+
